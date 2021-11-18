ce0069482cba44c5c6716658748bf7f1f705f72a
anitahb80 <anitahb@uia.no>
2021-11-18 14:54:17 +0100

    fikset connection string

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 //TODO
 
+//setup connection ---------------------
+const todo = require('./modules/todo.js'); 
+
+const pg = require('pg');
+const dbURI = "postgres://jlpjzmkfcwxgfa:da48aedd015f3dabc38773136eb7ff0745a414d95cbf7a9c607f75a12c1a090e@ec2-52-31-219-113.eu-west-1.compute.amazonaws.com:5432/dd7qf9370fk6to";
+const connstring = process.env.DATABASE_URL || dbURI;
+const pool = new pg.Pool({
+	connectionString: connstring,
+	ssl: {rejectUnauthorized: false}
+});
+
 const express = require("express");
 const server = express();
 const PORT = process.env.PORT || 8080;

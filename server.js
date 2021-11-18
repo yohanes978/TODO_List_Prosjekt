9a2defb530f18980df1b56aee570c5cd312df205
anitahb80 <anitahb@uia.no>
2021-11-18 14:11:03 +0100

    fikset server.js merge

--- server.js
+++ server.js
@@ -10,30 +10,18 @@ server.use(express.static("public"));
 server.use(express.json());
 
 // endpoints ----------------------------
-<<<<<<< HEAD
 server.get("/grayrubiousmagyarosaurus", function(req, res, next) {
 	res.status(200).send("Hello from GET").end();
 });
 
 server.post("/grayrubiousmagyarosaurus", function(req, res, next) {	
-=======
-server.get("/", function(req, res, next) {
-	res.status(200).send("Hello from GET").end();
-});
-
-server.post("/", function(req, res, next) {	
->>>>>>> 6690df74fc295d08002e2c2f3f1f89d38ff01074
 	console.log(req.body.country);
 	res.status(200).send("Hello from POST").end();
 });
 
 
 
-<<<<<<< HEAD
 server.delete("/grayrubiousmagyarosaurus", function(req, res, next) {
-=======
-server.delete("/", function(req, res, next) {
->>>>>>> 6690df74fc295d08002e2c2f3f1f89d38ff01074
 	res.status(200).send("Hello from DELETE").end();
 });
 

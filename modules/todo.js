b6d0aa2f876e40cfb0dc85135ba2e809c996a1b5
TheLovelyLaugh <thelovelylaugh@gmail.com>
2021-11-18 14:05:49 +0100

    Restaurert mappestruktur

--- modules/todo.js
+++ modules/todo.js
@@ -1,7 +1,6 @@
 //setup connection ---------------------
 const pg = require('pg');
-const dbURI = "postgres://jlpjzmkfcwxgfa:da48aedd015f3dabc38773136eb7ff0745a414d95cbf7a9c607f75a12c1a090e@ec2-52-31-219-113.eu-west-1.compute.amazonaws.com:5432/dd7qf9370fk6to
-";
+const dbURI = "postgres://jlpjzmkfcwxgfa:da48aedd015f3dabc38773136eb7ff0745a414d95cbf7a9c607f75a12c1a090e@ec2-52-31-219-113.eu-west-1.compute.amazonaws.com:5432/dd7qf9370fk6to";
 const connstring = process.env.DATABASE_URL || dbURI;
 const pool = new pg.Pool({
 	connectionString: connstring,
@@ -15,14 +14,14 @@ let dbMethods = {}; //create empty object
 //----------------------------------------
 
 dbMethods.getAllTodoLists = function(){
-    let sql = "SELECT * FROM todolists";
+    let sql = "SELECT * FROM grayrubiousmagyarosaurus";
     return pool.query(sql); //return the promise
 
 }
 
 //--------------------------------------
 dbMethods.createTodoList = function(heading, blogtext, userid) {
-    let sql = 'INSERT INTO todolists (id, date, heading, blogtext, userid) VALUES(DEFAULT, DEFAULT, $1, $2, $3) returning*';
+    let sql = 'INSERT INTO grayrubiousmagyarosaurus (id, date, heading, blogtext, userid) VALUES(DEFAULT, DEFAULT, $1, $2, $3) returning*';
 	let values = [updata.heading, updata.blogtext, userid];
     return pool.query(sql, values); //return the promise
 
@@ -30,7 +29,7 @@ dbMethods.createTodoList = function(heading, blogtext, userid) {
 
 //-------------------------------------
 dbMethods.deleteBlogPost = function(id){
-    let sql = "DELETE FROM todolists WHERE id = $1 RETURNING *";
+    let sql = "DELETE FROM grayrubiousmagyarosaurus WHERE id = $1 RETURNING *";
     let values = [id];
     return pool.query(sql, values); //return the promise
     

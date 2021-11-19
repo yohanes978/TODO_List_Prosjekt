59a6ace2379f114680aeda631b6908fbf95fd4f0
anitahb80 <anitahb@uia.no>
2021-11-19 12:11:46 +0100

    fikset routing

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const todo = require('./modules/todo.js');
 const pg = require('pg');
 const dbURI = "postgres://jlpjzmkfcwxgfa:da48aedd015f3dabc38773136eb7ff0745a414d95cbf7a9c607f75a12c1a090e@ec2-52-31-219-113.eu-west-1.compute.amazonaws.com:5432/dd7qf9370fk6to";
 const connstring = process.env.DATABASE_URL || dbURI;
+const router = require("./modules/todolists.js")
 const pool = new pg.Pool({
 	connectionString: connstring,
 	ssl: {rejectUnauthorized: false}
@@ -19,22 +20,9 @@ server.set("port", PORT);
 // middleware ---------------------------
 server.use(express.static("public"));
 server.use(express.json());
+server.use(router)
 
 // endpoints ----------------------------
-server.get("/grayrubiousmagyarosaurus", function(req, res, next) {
-	res.status(200).send("Hello from GET").end();
-});
-
-server.post("/grayrubiousmagyarosaurus", function(req, res, next) {	
-	console.log(req.body.country);
-	res.status(200).send("Hello from POST").end();
-});
-
-
-
-server.delete("/grayrubiousmagyarosaurus", function(req, res, next) {
-	res.status(200).send("Hello from DELETE").end();
-});
 
 
 // start server
